94adce1f718de43d06f86b011d016ea41ea8acf4
Nattavit Kamoltham <kamoltham.n.t@gmail.com>
2019-08-31 14:35:08 +0800

    update webpack css loader

--- webpack.config.js
+++ webpack.config.js
@@ -23,6 +23,10 @@ module.exports = {
 				test: /\.less$/,
 				use: ['style-loader', 'css-loader', 'less-loader'],
 			},
+			{ 
+				test: /\.css$/, 
+				loader: 'style-loader!css-loader' 
+			},
 		],
 	},
 	plugins: [
